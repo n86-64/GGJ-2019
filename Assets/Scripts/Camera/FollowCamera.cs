307f6d1d1a82878533c61de1dbe7016a333f43b0
Ellie Dunstan <ellie2.dunstan@live.uwe.ac.uk>
2019-01-25 22:50:27 +0000

    Auto stash before merge of "develop" and "camera"

--- Assets/Scripts/Camera/FollowCamera.cs
+++ Assets/Scripts/Camera/FollowCamera.cs
@@ -9,14 +9,32 @@ public class FollowCamera : MonoBehaviour
     public float speed = 1;
 
     private float distance = 3;
+    private Vector3 startPos;
 
     private void LateUpdate()
     {
-        transform.position = Vector3.Lerp(
-            transform.position, objToFollow.position,
-            Time.deltaTime * speed);
+        if (Vector3.Distance(transform.position, objToFollow.position) > 0.1f)
+        {
+            transform.position = Vector3.Lerp(
+                transform.position, objToFollow.position,
+                Time.deltaTime * speed);
+
+            //float t = Mathf.InverseLerp(
+            //    startPos.magnitude, objToFollow.position.magnitude,
+            //    transform.position.magnitude);
+            //t = Mathf.SmoothStep(0, 1, t);
+            //cam.orthographicSize = Mathf.Lerp(5, 3, Time.deltaTime * speed);
+        }
+        else
+        {
+            startPos = transform.position;
+            //cam.orthographicSize = Mathf.Lerp(3, 5, Time.deltaTime * speed);
+        }
+
         
-        distance = 3 + (Vector3.Distance(transform.position, objToFollow.position) / 2);
-        cam.orthographicSize = distance;
+        //Debug.Log(distance * Mathf.Sin(Time.deltaTime * speed));
+
+        //distance *= Mathf.Sin(Time.deltaTime * speed);//3 + (Vector3.Distance(transform.position, objToFollow.position) / 2);
+        
     }
 }
