d5cd91d2a063533e6663cdd4d88640d033a3c948
Ellie Dunstan <ellie2.dunstan@live.uwe.ac.uk>
2019-01-25 21:40:26 +0000

    Orthographic follow camera

---
+++ Assets/Scripts/Camera/FollowCamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    public Camera cam;
+    public Transform objToFollow;
+    public float speed = 1;
+
+    private float distance = 3;
+
+    private void LateUpdate()
+    {
+        transform.position = Vector3.Lerp(
+            transform.position, objToFollow.position,
+            Time.deltaTime * speed);
+        
+        distance = 3 + (Vector3.Distance(transform.position, objToFollow.position) / 2);
+        cam.orthographicSize = distance;
+    }
+}
