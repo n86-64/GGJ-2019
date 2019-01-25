d40e7bd1bbd16fc7f4775530461b2130109609fa
Nathan <nathan.butt@gmail.com>
2019-01-25 21:40:03 +0000

    Added basic menu selection.

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -8,7 +8,12 @@ public class MainMenu : MonoBehaviour {
     [SerializeField]
     private int menuSelection = 0;
 
+    private int prevMenuSelection = 0;
+
+
     public List<Button> menuButtons;
+    public List<GameObject> pointerPos;
+    public GameObject pointer;
 
 	// Use this for initialization
 	void Start ()
@@ -19,6 +24,7 @@ public class MainMenu : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
+        prevMenuSelection = menuSelection;
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             menuSelection++;
@@ -40,6 +46,10 @@ public class MainMenu : MonoBehaviour {
     {
         if (menuSelection == menuButtons.Count ) { menuSelection = 0; }
         else if(menuSelection < 0) { menuSelection = menuButtons.Count - 1; }
+
+
+        menuButtons[prevMenuSelection].GetComponent<Image>().color = Color.white;
+        menuButtons[menuSelection].GetComponent<Image>().color = Color.gray;
     }
 
 }
