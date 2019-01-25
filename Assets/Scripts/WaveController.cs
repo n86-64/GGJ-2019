1297ea6dcbfaa63544d0629032522fa959369e7b
Ellie Dunstan <ellie2.dunstan@live.uwe.ac.uk>
2019-01-25 20:25:53 +0000

    Yeet

---
+++ Assets/Scripts/WaveController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveController : MonoBehaviour
+{
+    public Wave[] waves;
+
+    public int currentWave = 0;
+
+
+    public void SpawnNextWave()
+    {
+        currentWave++;
+        Wave newWave = waves[currentWave];
+
+        for(int i = 0; i < newWave.numberToSpawn; i++)
+        {
+            GameObject unit = Instantiate(newWave.prefab);
+            //instantiate as an enemy type
+            //new list of units
+        }
+    }
+	
+}
