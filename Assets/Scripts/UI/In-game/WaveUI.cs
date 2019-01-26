2acf7cb7a3464ca6c2984f2df6e5cb307d3bcc83
Nathan <nathan.butt@gmail.com>
2019-01-26 00:07:55 +0000

    Added last commit dependancies

---
+++ Assets/Scripts/UI/In-game/WaveUI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaveUI : MonoBehaviour
+{
+    private WaveController waveController;
+
+	// Use this for initialization
+	void Start ()
+    {
+        waveController = FindObjectOfType<WaveController>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (waveController)
+        {
+            GetComponent<Text>().text = "Wave: " + (waveController.currentWave + 1) + "/" + waveController.waves.Length;
+        }
+	}
+}
