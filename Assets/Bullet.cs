7cca5dfa06fa4879e128ce2d3cf1a493ce436845
Ellie Dunstan <ellie2.dunstan@live.uwe.ac.uk>
2019-01-26 10:49:23 +0000

    Turret boii

---
+++ Assets/Bullet.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 origin;
+    public float bulletSpeed = 0.5f;
+    
+
+    private void OnEnable()
+    {
+        transform.position = origin;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag != "Weapon")
+        {
+            Debug.Log("Bullet hit something");
+            gameObject.SetActive(false);
+        }
+    }
+    private void Update()
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        transform.position += direction * Time.deltaTime * bulletSpeed;
+    }
+}
