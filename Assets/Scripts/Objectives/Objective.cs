3b0e34c836f8cbafe77fd40484372e1fe0325334
Nathan <nathan.butt@gmail.com>
2019-01-26 12:29:59 +0000

    Improved central objective.

--- Assets/Scripts/Objectives/Objective.cs
+++ Assets/Scripts/Objectives/Objective.cs
@@ -5,37 +5,35 @@ using UnityEngine;
 public class Objective : MonoBehaviour {
 
     [SerializeField]
-    private int health = 100;
+    private float health = 100;
+    public float maxHealth = 100;
 
 	// Use this for initialization
 	void Start ()
     {
-		
+        health = maxHealth;
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-		
-	}
-
-    void OnTriggerEnter(Collider other)
-    {
-        if(other.tag == "Enemy")
+		if(health <= 0.0f || Input.GetKeyDown(KeyCode.K))
         {
-            TakeDamage(other.GetComponent<Enemy>());   
-            // Just disable the enemy.
-        }    
-    }
+            Dead();
+        }
+	}
 
     void Dead()
     {
         // Im Dead end the game.
+        Debug.Log("Were dead mate"); 
         return;
     }
 
-    void TakeDamage(Enemy enemy)
+    public void TakeDamage(float damage)
     {
         // Here we will take damage. 
+        health -= damage;
+        health = Mathf.Clamp(health, 0.0f, maxHealth);
     }
 }
